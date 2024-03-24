996b8c10599304635257db79daad2b640e126e67
AnisHanif0100 <Anishanif21@gmail.com>
2024-03-24 17:06:33 +0500

    create a cli number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome To Cli-Number-Guessing-Game.");
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! you guess a right number.");
+}
+else {
+    console.log("You guess a wrong number.");
+}
